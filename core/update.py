90f3f6d664a25df67b42a5ea6e3b4ee8d1704d4c
SpookySec <anonymousbot100110@gmail.com>
2020-11-03 21:07:20 +0300

    added update.sh

--- core/update.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-
-def update():
-    process = subprocess.Popen(["git", "pull"], stdout=subprocess.PIPE)
-    output = process.communicate()[0]
-    return str(output.decode("utf-8")).strip()
