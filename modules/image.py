70a67401598fe67c0b502b4eff6732225fa73e30
SpookySec <anonymousbot100110@gmail.com>
2020-11-03 15:01:16 +0300

    update

---
+++ modules/image.py
@@ -0,0 +1,27 @@
+from PIL import Image
+
+def decode(img):
+    image = Image.open(img, 'r')
+ 
+    data = ''
+    imgdata = iter(image.getdata())
+ 
+    while (True):
+        pixels = [value for value in imgdata.__next__()[:3] +
+                                imgdata.__next__()[:3] +
+                                imgdata.__next__()[:3]]
+ 
+        # string of binary data
+        binstr = ''
+ 
+        for i in pixels[:8]:
+            if (i % 2 == 0):
+                binstr += '0'
+            else:
+                binstr += '1'
+ 
+        data += chr(int(binstr, 2))
+        if (pixels[-1] % 2 != 0):
+            return data
+
+print(decode(input("path to image with extention: ")))
